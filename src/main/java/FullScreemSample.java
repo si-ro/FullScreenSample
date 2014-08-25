998d411ca217066df7db100744076d62ea1725a8
sakaguchi manabu <sakaguchi.manab@jp.fujitsu.com>
2014-08-25 23:44:27 +0900

    initial commit.

---
+++ src/main/java/FullScreemSample.java
@@ -0,0 +1,30 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Window;
+
+import javax.swing.JFrame;
+
+
+public class FullScreemSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GraphicsDevice gs = GraphicsEnvironment.getLocalGraphicsEnvironment()
+				.getDefaultScreenDevice();
+		JFrame frame = new JFrame(gs.getDefaultConfiguration());
+		Window win = new Window(frame);
+		Canvas c = new Canvas();
+		c.setBackground(Color.RED);
+		win.add(c);
+		win.show(); // or setVisible(true);
+
+		// Enter full-screen mode
+		gs.setFullScreenWindow(win);
+		win.validate();
+	}
+
+}

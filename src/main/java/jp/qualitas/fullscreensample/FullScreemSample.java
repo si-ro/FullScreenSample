0778cc016ad4c0a263b5f37046573384d2c26fe7
sakaguchi manabu <sakaguchi.manab@jp.fujitsu.com>
2014-08-27 00:38:49 +0900

    append JavaFX Sample class.

--- src/main/java/jp/qualitas/fullscreensample/FullScreemSample.java
+++ src/main/java/jp/qualitas/fullscreensample/FullScreemSample.java
@@ -1,3 +1,4 @@
+package jp.qualitas.fullscreensample;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.GraphicsDevice;
---
+++ src/main/java/jp/qualitas/fullscreensample/javafx/FullScreenSampleJavaFX.java
@@ -0,0 +1,22 @@
+package jp.qualitas.fullscreensample.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FullScreenSampleJavaFX extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
